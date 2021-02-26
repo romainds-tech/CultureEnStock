13fa6b76911cb9d75ecf2e31bc94a686a102b1f8
romainds-tech <romain.da-silva@etu.univ-smb.fr>
2021-02-26 09:06:23 +0100

    TailWindCss Installation

--- CultureEnStock/resources/views/home.blade.php
+++ CultureEnStock/resources/views/home.blade.php
@@ -3,19 +3,21 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,
-initial-scale=1">
-<title>Laravel</title>
-<link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-<script src="{{ mix('/js/app.js') }}" defer></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Laravel</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
+
 <body>
-<div id="app">
-<example-component></example-component>
-</div>
+    <div id="app">
+        <example-component></example-component>
+    </div>
 </body>
+
 </html>
 
 
