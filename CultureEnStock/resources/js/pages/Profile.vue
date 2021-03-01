b5e3de3d8bb1d3e414697a462e3b23b8513c0c83
ClaireAmor <claireamor7033@gmail.com>
2021-03-01 11:54:57 +0100

    claire a change le profile

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10">
-    <div class="flex space-x-4">
+  <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
+    <div class="flex justify-between space-x-4 mt-24">
       <div
-        class="flex-1 rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-6">
+        class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full ">
             photo
       </div>
-      <div
-        class="flex-1 ">
 
-        </div>
       <div
-        class="flex-1 rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-6">
+        class=" rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-6">
             PSEUDO
       </div>
     </div>
-  </div>
+  <!-- </div> -->
 </template>
 
 
-</script>
