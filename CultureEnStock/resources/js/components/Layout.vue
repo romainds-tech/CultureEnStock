673b2a795b968b0e989f346eb2967c1e2dc372fb
MorganeK11 <morganekeller11@gmail.com>
2021-03-01 11:04:35 +0100

    Layout.vue

--- CultureEnStock/resources/js/components/Layout.vue
+++ CultureEnStock/resources/js/components/Layout.vue
@@ -3,33 +3,39 @@
   <div class="container">
 
 
-      <!-- TON CODE A ADAPTER -->
-      <div class="content_header" style="display: flex; flex-direction: row;">
+        <!-- TON CODE A ADAPTER -->
+        <div class="content_header bg-green-200 flex py-4 align-middle">
 
-            <div style="padding: 20px;">
-                <img src="" class="log"/>
-                <p>Logo</p>
+            <router-link to="/">
+                <div class="flex-none w-40 text-center">
+                    <img src="" class="log"/>
+                    <p>Logo</p>
+                </div>
+            </router-link>
+
+            <div class="flex-grow flex-wrap flex text-center border-l-2 border-r-2">
+                <button class="flex-1 px-2 text-center underline whitespace-nowrap">Réservation de la salle</button>
+                <button class="flex-1 px-2 text-center underline whitespace-nowrap">Evènement</button>
+                <button class="flex-1 px-2 text-center underline whitespace-nowrap">Réservation d'un billet</button>
             </div>
 
-            <div style="padding: 20px;">
-                <button>Réservation de la salle</button>
-                <button>Evènement</button>
-                <button>Réservation d'un billet</button>
-            </div>
-
-            <div style="padding: 20px;">
-                <img src="" class="photo-profil"/>
-                <p class="pseudo">Pseudo</p>
+            <div class="flex-none w-40 text-center">
+                <router-link to="/login">Se connecter</router-link>
+                <!--Si connecté
+                <router-link to="/profile">
+                    <img src="" class="photo-profil"/>
+                    <p class="pseudo">Pseudo</p>
+                 </router-link> -->
             </div>
 
         </div>
 
-    <!-- TON CODE A ADAPTER -->
+        <!-- TON CODE A ADAPTER -->
 
-
+    <!--
     <router-link to="/"> Home </router-link>
     <router-link to="/login"> Login </router-link>
-    <router-link to="/profile"> Profil </router-link>
+    <router-link to="/profile"> Profil </router-link> -->
 
     <router-view></router-view>
   </div>
