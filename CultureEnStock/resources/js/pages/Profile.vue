b097902eded14b3e3fcabe0f75a8b76e2cd2a9d4
ClaireAmor <claireamor7033@gmail.com>
2021-03-01 11:56:33 +0100

    claire a changé le profil

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
-    <div class="flex justify-between space-x-4 mt-24">
+    <div class="container flex justify-between space-x-4 mt-24">
       <div
         class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full ">
             photo
