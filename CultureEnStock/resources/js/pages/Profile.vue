1438fa89553f8dd9cf5d3d202bc6ae561c95433a
ClaireAmor <claireamor7033@gmail.com>
2021-03-01 11:03:04 +0100

    claire a changer le profile

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,3 +1,26 @@
 <template>
+  <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10">
+    <div class="flex space-x-4">
+      <div
+        class="flex-1 rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-6">
+            photo
+      </div>
+      <div
+        class="flex-1 ">
 
+        </div>
+      <div
+        class="flex-1 rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-6">
+            PSEUDO
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import VueResizable from "vue-resizable";
+
+export default {
+
+}
+</script>
