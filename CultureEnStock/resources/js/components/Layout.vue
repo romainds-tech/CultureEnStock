cce39d426f99d6c2cf27819e58e5085d24d6ed3c
MorganeK11 <morganekeller11@gmail.com>
2021-03-02 17:07:51 +0100

    Changement de nom boutons

--- CultureEnStock/resources/js/components/Layout.vue
+++ CultureEnStock/resources/js/components/Layout.vue
@@ -1,54 +1,63 @@
+<!-- Morgane -->
+
 <template>
-    <div>
-        <!-- TON CODE A ADAPTER -->
-        <div class="content_header bg-green-200 flex py-4 align-middle mb-20">
+  <div>
+    <!-- TON CODE A ADAPTER -->
+    <div class="content_header bg-green-200 flex py-4 align-middle mb-20">
+      <router-link to="/">
+        <div class="flex-none w-80 text-center">
+          <img src="" class="log" />
+          <p>Logo</p>
+        </div>
+      </router-link>
 
-            <router-link to="/">
-                <div class="flex-none w-80 text-center">
-                    <img src="" class="log"/>
-                    <p>Logo</p>
-                </div>
-            </router-link>
+      <div
+        class="flex-grow flex-wrap flex text-center border-l-2 border-r-2 border-gray-800"
+      >
+        <router-link
+          class="flex-1 px-2 text-center underline whitespace-nowrap"
+          to=""
+        >
+          Créer un évènement</router-link
+        >
+        <router-link
+          class="flex-1 px-2 text-center underline whitespace-nowrap"
+          to=""
+        >
+          Nos spectacles</router-link
+        >
+        <router-link
+          class="flex-1 px-2 text-center underline whitespace-nowrap"
+          to="/reservationBillet"
+        >
+          Délivrer un ticket</router-link
+        >
+      </div>
 
-            <!--
-
-                Créer évènement : Formulaire ajout évènement + réservation salle
-
-                Nos spectacles : Liste des évènements avec bouton réservation billet
-
-            -->
-
-            <div class="flex-grow flex-wrap flex text-center border-l-2 border-r-2 border-gray-800">
-                <button class="flex-1 px-2 text-center underline whitespace-nowrap">Réservation de la salle</button>
-                <button class="flex-1 px-2 text-center underline">Evènement</button>
-                <router-link class="flex-1" to="/reservationBillet">Réservation d'un billet</router-link>
-            </div>
-
-
-            <div class="flex-none flex w-80 text-center">
-                <!-- Si pas connecté-->
+      <div class="flex w-80 text-center">
+        <!-- Si pas connecté-->
                 <router-link to="/login" class="flex-1">Se connecter</router-link>
                 <router-link to="/register" class="flex-1">S'inscrire</router-link>
-                <!-- Si connecté
-                <router-link to="/profile">
-                    <img src="" class="photo-profil"/>
-                    <p class="pseudo">Pseudo</p>
-                 </router-link> -->
-            </div>
-        </div>
-        <!-- TON CODE A ADAPTER -->
-        <!--
+        <!-- Si connecté
+        <router-link to="/profile">
+          <img src="" class="" />
+          <p>Pseudo</p>
+        </router-link>-->
+      </div>
+    </div>
+    <!-- TON CODE A ADAPTER -->
+    <!--
         <router-link to="/"> Home </router-link>
         <router-link to="/login"> Login </router-link>
         <router-link to="/profile"> Profil </router-link> -->
-        <router-view></router-view>
-    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log("Component mounted.");
-        },
-    };
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
 </script>
