a19b39bd7ab1aa98d27e1194e49b380321efac9b
ClaireAmor <claireamor7033@gmail.com>
2021-03-02 17:09:18 +0100

    claire a modifier un peu le profile

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,16 +1,25 @@
 <template>
   <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
-    <div class="container flex justify-between space-x-4 mt-24">
+  <div class="container">
+    <div class="flex justify-between space-x-4 mt-24">
       <div
-        class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full ">
-            photo
+        class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full"
+      >
+        photo
       </div>
 
       <div
-        class=" rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-96">
-            PSEUDO
+        class="rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-96"
+      >
+        PSEUDO
       </div>
     </div>
+
+    <div class="items-stretch flex">
+      <div class="">S'abonner</div>
+      <div>Modifier</div>
+    </div>
+  </div>
   <!-- </div> -->
 </template>
 
