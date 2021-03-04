2296a7a735f00f1c53aefe694e9f30ac557bb1b0
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 11:37:02 +0100

    Event Component

---
+++ CultureEnStock/resources/js/components/Evenement.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-1/2 m-auto border-2">
+        <div class="eventtitle text-center p-2">
+            {{ libelle }}
+        </div>
+        <div class="eventdesc p-2 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, labore voluptas repellat, quos animi inventore vitae consequuntur odit reprehenderit molestiae autem suscipit optio praesentium eos distinctio iste dignissimos corrupti dolore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusantium laudantium unde officiis enim non. Similique dolorum eligendi id necessitatibus ad cumque recusandae aliquam obcaecati distinctio nostrum? Architecto, consectetur explicabo?</div>
+        <div class="eventdate p-2">Le {{ datedebut }}</div>
+        <div class="eventprice p-2 w-full text-right"> 50 €</div>
+        <button class="btn-reserve block m-auto p-2 border-2 bg-gray-300">Reserver</button>
+    </div>
+</template>
+<script>
+export default {
+    props: ["idevenement", "idtype", "libelle", "datedebut"],
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
--- CultureEnStock/resources/js/pages/events/ListEvent.vue
+++ CultureEnStock/resources/js/pages/events/ListEvent.vue
@@ -1,16 +1,31 @@
 <template>
     <div class="container">
-
-        <!-- v-for='' -->
         <div class="w-1/2 m-auto border-2">
-            <div class="eventtitle text-center p-2">
-                Evenement
-            </div>
-            <div class="eventdesc p-2 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, labore voluptas repellat, quos animi inventore vitae consequuntur odit reprehenderit molestiae autem suscipit optio praesentium eos distinctio iste dignissimos corrupti dolore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusantium laudantium unde officiis enim non. Similique dolorum eligendi id necessitatibus ad cumque recusandae aliquam obcaecati distinctio nostrum? Architecto, consectetur explicabo?</div>
-            <div class="eventdate p-2"> du 12 au 15 Septembre</div>
-            <div class="eventprice p-2 w-full text-right"> 50 €</div>
-            <button class="btn-reserve block m-auto p-2 border-2 bg-gray-300">Reserver</button>
+            <event
+                v-for="(event, index) in events"
+                :key="index"
+                :id="event.idevenement"
+                :idtype="event.idtype"
+                :libelle="event.libelle"
+                :datedebut="event.date_debut"
+            ></event>
         </div>
-
     </div>
 </template>
+
+<script>
+import Evenement from '../../components/Evenement.vue';
+
+export default {
+    components: {
+        "event": Evenement
+    },
+
+    mounted() {
+        axios.get("/api/evenement")
+            .then((response) => {
+                this.events = response.data.data;
+            })
+    },
+}
+</script>
