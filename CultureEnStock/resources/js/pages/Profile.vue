52c0c042618cb5d81ac562d52a25faa23cae0521
ClaireAmor <claireamor7033@gmail.com>
2021-03-09 11:26:46 +0100

    claire modifie le profil

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,23 +1,40 @@
 <template>
   <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
   <div class="container">
-    <div class="flex justify-between space-x-4 mt-24">
-      <div
-        class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full"
-      >
-        photo
+    <div class="border-b-2 border-purple-800">
+      <div class="flex justify-between space-x-4 mt-24">
+        <div
+          class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full"
+        >
+          photo
+        </div>
+
+        <div
+          class="rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-96"
+        >
+          PSEUDO
+        </div>
       </div>
 
-      <div
-        class="rounded-md bg-pink-900 text-white font-semibold flex items-center justify-center py-3 px-96"
-      >
-        PSEUDO
+      <div class="items-stretch flex text-center space-x-4 justify-end my-5">
+        <div class="rounded-full w-28 h-8 bg-yellow-500">S'abonner</div>
+        <div class="rounded-full w-28 h-8 bg-red-700">Modifier</div>
       </div>
     </div>
 
-    <div class="items-stretch flex">
-      <div class="">S'abonner</div>
-      <div>Modifier</div>
+    <div class="mt-20 w-9/12 ml-20">
+      <div class="border-b-2  border-purple-800">
+        <div>PSEUDO</div>
+      </div>
+      <div class="items-stretch">
+        <div>NOM</div>
+        <div>PRENOM</div>
+      </div>
+
+      <div class="items-stretch flex text-center space-x-4 justify-end my-5">
+        <div class="rounded-full w-28 h-8 bg-yellow-500">S'abonner</div>
+        <div class="rounded-full w-28 h-8 bg-red-700">Modifier</div>
+      </div>
     </div>
   </div>
   <!-- </div> -->
