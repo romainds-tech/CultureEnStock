6fdff0dbe38382cd89e45f2b5aa92e14edf07850
ClaireAmor <claireamor7033@gmail.com>
2021-03-09 11:49:41 +0100

    claire à change profile

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
   <div class="container">
-    <div class="border-b-2 border-purple-800">
+    <div class="border-b-4 border-purple-800">
       <div class="flex justify-between space-x-4 mt-24">
         <div
           class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full"
@@ -22,20 +22,22 @@
       </div>
     </div>
 
-    <div class="mt-20 w-9/12 ml-20">
+    <div class="mt-20 w-9/12 ml-20 m-10">
       <div class="border-b-2  border-purple-800">
         <div>PSEUDO</div>
       </div>
-      <div class="items-stretch">
-        <div>NOM</div>
-        <div>PRENOM</div>
+      <div class="items-stretch flex justify-between">
+        <div class="w-1/4 border-b-2  border-pink-800">NOM</div>
+        <div class="w-1/4 border-b-2  border-pink-800">PRENOM</div>
+      </div>
+      <div class="border-b-2  border-purple-800">
+        <div>PSEUDO</div>
       </div>
 
-      <div class="items-stretch flex text-center space-x-4 justify-end my-5">
-        <div class="rounded-full w-28 h-8 bg-yellow-500">S'abonner</div>
-        <div class="rounded-full w-28 h-8 bg-red-700">Modifier</div>
-      </div>
     </div>
+    <div class="items-stretch flex text-center space-x-4 justify-end my-5">
+        <div class="rounded-full w-44 h-8 bg-green-600">Mes réservations</div>
+      </div>
   </div>
   <!-- </div> -->
 </template>
