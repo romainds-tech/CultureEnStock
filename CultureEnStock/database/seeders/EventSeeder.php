9013800b62f3fe8bc9ab46ab1a063256f276bcbd
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 08:16:52 +0100

    Seeding ^^

---
+++ CultureEnStock/database/seeders/EventSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('evenement')->insert([
+            'idtype' => 2,
+            'libelle' => 'Frozen',
+            'tmp_instal' => 20,
+            'tmp_desinstal' => 30,
+            'tmp_netoy' => 20,
+            'date_debut' => Carbon::create('2021', '04', '23')
+        ]);
+    }
+}
