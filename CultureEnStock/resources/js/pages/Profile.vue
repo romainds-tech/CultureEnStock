1b82d3b44d3b2b2ff92fc37298252d6e13853ddc
ClaireAmor <claireamor7033@gmail.com>
2021-03-16 10:42:27 +0100

    claire a rajouter le formulaire sur le profil

--- CultureEnStock/resources/js/pages/Profile.vue
+++ CultureEnStock/resources/js/pages/Profile.vue
@@ -1,7 +1,8 @@
 <template>
+  <!-- Claire -->
   <!-- <div class="rounded-xl overflow-hidden bg-pink-400 to-teal-100 p-10 mt-24"> -->
   <div class="container">
-    <div class="border-b-4 border-purple-800">
+    <div class="border-b-4 border-purple-800 mb-20">
       <div class="flex justify-between space-x-4 mt-24">
         <div
           class="bg-pink-900 h-44 w-44 text-white text-center font-extrabold flex items-center justify-center rounded-full"
@@ -22,22 +23,64 @@
       </div>
     </div>
 
-    <div class="mt-20 w-9/12 ml-20 m-10">
-      <div class="border-b-2  border-purple-800 mb-4">
+    <!-- <div class="mt-20 w-9/12 ml-20 m-10">
+      <div class="border-b-2 border-purple-800 mb-4">
         <div>PSEUDO</div>
       </div>
       <div class="items-stretch flex justify-between mb-4">
-        <div class="w-1/4 border-b-2  border-pink-800">NOM</div>
-        <div class="w-1/4 border-b-2  border-pink-800">PRENOM</div>
+        <div class="w-1/4 border-b-2 border-pink-800">NOM</div>
+        <div class="w-1/4 border-b-2 border-pink-800">PRENOM</div>
       </div>
-      <div class="border-b-2  border-purple-800 mb-4">
+      <div class="border-b-2 border-purple-800 mb-4">
         <div>PSEUDO</div>
       </div>
+    </div> -->
 
+    <div class="card w-2/3 m-auto rounded-3xl">
+      <form id="profil-form" @submit="checkProfilForm" action="/" method="POST">
+        <div class="container p-4">
+          <input
+            class="username-box p-2 input-xl w-full mb-6"
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Pseudo"
+            disabled
+          />
+          <br />
+          <div class="flex justify-between ">
+            <input
+              class="lastname-box p-2 w-1/3"
+              type="text"
+              id="lastname"
+              name="lastname"
+              placeholder="Nom"
+              disabled
+            />
+            <input
+              class="name-box p-2 w-1/3"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Prénom"
+              disabled
+            />
+          </div>
+          <br />
+          <input
+            class="mail-box p-2 input-xl w-full "
+            type="mail"
+            id="mail"
+            name="mail"
+            placeholder="Mail"
+            disabled
+          />
+        </div>
+      </form>
     </div>
     <div class="items-stretch flex text-center space-x-4 justify-end my-5">
-        <div class="rounded-full w-44 h-8 bg-green-600">Mes réservations</div>
-      </div>
+      <div class="rounded-full w-44 h-8 bg-green-600">Mes réservations</div>
+    </div>
   </div>
   <!-- </div> -->
 </template>
